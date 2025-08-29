3d9282ccf702b250365435879bc0f207551df5eb
José Miguel Trinidad R. <jm.trinidad.99@hotmail.com>
2025-08-29 14:58:23 -0400

    Create ExampleListAndSetMap.java

---
+++ First-Cl/src/main/java/com/coursojava/sesion07/ExampleListAndSetMap.java
@@ -0,0 +1,29 @@
+package com.coursojava.sesion07;
+
+import java.util.*;
+
+public class ExampleListAndSetMap {
+
+    public static void main(String[] args) {
+
+        //ES IGUAL QUE LA LISTA PERO EN ESTE CASO ES UN CONJUNTO DE DATOS NO
+        //ORDENADOS
+        //LAS LISTAS SON UN CONJUNTO DE DATOS ORDENADOS
+        Set<String> conjunto = new HashSet<String>();
+
+        //ESTA ES LA FORMA DE INICIALIZAR UNA LISTA
+        List<String> Listado = new ArrayList<>();
+
+        //LOS MAPAS SON PARECIDOS O PRACTICAMENTE ES LO MISMO QUE UN Dictionary EN C#
+        //CLAVE VALOR
+        Map<Integer, String> mapa= new HashMap<>();
+
+        mapa.put(1, "Test");
+
+        for (Map.Entry<Integer, String> entry : mapa.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+
+    }
+}
